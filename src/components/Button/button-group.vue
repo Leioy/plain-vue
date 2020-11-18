5dbfa74207b4efc9897541faa790631991c316c3
Leioy <Leioy9312@gmail.com>
2020-11-18 21:49:20 +0800

    feat: button-group组件，修复button样式

---
+++ src/components/Button/button-group.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="pl-button-group">
+		<slot></slot>
+	</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'PlButtonGroup',
+})
+</script>
+<style>
+</style>
